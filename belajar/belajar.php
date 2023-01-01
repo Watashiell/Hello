9f560986ed54ae641c3df219cc48a1dfdc5d2cee
WishingWell <90557594+Watashiell@users.noreply.github.com>
2023-01-01 13:16:10 +0700

    Add files via upload

---
+++ belajar/belajar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP5</title>
+</head>
+
+
+<body>
+
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <label for="file">Nama File:</label>
+        <input type="file" name="file" id="file"><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+
+</html>
